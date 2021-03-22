ae1607156e124ad10ec7243269d6fc554dfab6b3
zainzulifqargit <zainzulifqar21@gmail.com>
2021-03-22 17:06:50 +0500

    wip

--- app/Http/Livewire/IdeaComment.php
+++ app/Http/Livewire/IdeaComment.php
@@ -3,8 +3,6 @@
 namespace App\Http\Livewire;
 
 use App\Models\Comment;
-use App\Models\Idea;
-use Illuminate\Support\Facades\Log;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -14,7 +12,7 @@ class IdeaComment extends Component
 
     public $idea;
     public $comment;
-    public $perPage = 2;
+    public $perPage = 8;
 
     protected $listeners = ['comment-saved' => 'reRender'];
 
@@ -30,12 +28,12 @@ class IdeaComment extends Component
 
     public function reRender()
     {
-        // 
+        //
     }
 
     public function updateComment($data)
     {
-        if (! auth()->check()) {
+        if (!auth()->check()) {
             return redirect('/login');
         }
 
@@ -45,16 +43,15 @@ class IdeaComment extends Component
             $comment->body = $data['body'];
             $comment->update();
 
-            session()->flash('success','Comment Updated Successfully');
-        }
-        else {    
-            session()->flash('error','Whoops Something Went Wrong');
+            session()->flash('success', 'Comment Updated Successfully');
+        } else {
+            session()->flash('error', 'Whoops Something Went Wrong');
         }
     }
 
     public function deleteComment(Comment $comment)
     {
-        if (! auth()->check()) {
+        if (!auth()->check()) {
             return redirect('/login');
         }
 
@@ -62,19 +59,17 @@ class IdeaComment extends Component
             $comment->delete();
 
             $this->emit('comment-deleted');
-            session()->flash('success','Comment Deleted Successfully');
-        }
-        else
-        {
-            session()->flash('error','Un-authorized Action');
+            session()->flash('success', 'Comment Deleted Successfully');
+        } else {
+            session()->flash('error', 'Un-authorized Action');
         }
     }
 
     public function render()
     {
-        return view('livewire.idea-comment',[
-            'idea' => $this->idea,
-            'comments' => Comment::with('user')->where('idea_id',$this->idea->id)->orderBy('id','desc')->paginate($this->perPage),
+        return view('livewire.idea-comment', [
+            'idea'     => $this->idea,
+            'comments' => Comment::with('user')->where('idea_id', $this->idea->id)->orderBy('id', 'desc')->paginate($this->perPage),
         ]);
     }
 }
