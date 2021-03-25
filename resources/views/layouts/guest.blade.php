b1f3a670ad75d838fe70bf4dd968813503875ed8
zainzulifqargit <zainzulifqar21@gmail.com>
2021-03-25 08:38:29 +0500

    wip

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,7 +16,7 @@
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
 
-        {{ $style }}
+        {{ $style ?? '' }}
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
