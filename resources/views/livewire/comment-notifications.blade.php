f604a81fac716e479e80ea449ffd2d5fc90d329f
Andre Madarang <andre_madarang@hotmail.com>
2021-05-29 02:56:03 -0400

    Episode 63 - Notification Polling

--- resources/views/livewire/comment-notifications.blade.php
+++ resources/views/livewire/comment-notifications.blade.php
@@ -1,4 +1,8 @@
-<div x-data="{ isOpen: false }"class="relative" >
+<div
+    wire:poll="getNotificationCount"
+    x-data="{ isOpen: false }"
+    class="relative"
+>
     <button @click=
         "isOpen = !isOpen
         if (isOpen) {
